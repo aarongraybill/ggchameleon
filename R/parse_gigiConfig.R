779aa77fbfc65fbc6aa22b17f29e93be3c71f8b6
Aaron Graybill <aarongraybill12@gmail.com>
2023-03-18 08:41:36 -0400

    handle file not found in config parser

--- R/parse_gigiConfig.R
+++ R/parse_gigiConfig.R
@@ -5,8 +5,15 @@ parse_configs <- function(config = "default",file="gigiConfig.yml") {
       call. = FALSE
     )
   }
-  configs <- config::get(config = config,file=file)
-  rlang::exec("edit_the_main_palette",!!!configs$main_palette)
-  rlang::exec("edit_the_accent_palette",!!!configs$accent_palette)
-  rlang::exec("edit_the_fonts",!!!configs$fonts)
+  if (file.exists(file)){
+    configs <- config::get(config = config,file=file)
+    rlang::exec("edit_the_main_palette",!!!configs$main_palette)
+    rlang::exec("edit_the_accent_palette",!!!configs$accent_palette)
+    rlang::exec("edit_the_fonts",!!!configs$fonts)
+  } else if (file!="gigiConfig.yml"){
+    message(paste0("File \"",file,"\" not found. Resorting to existing. configs."))
+  } else{
+    # Default config file and not found, must not be using configs
+    # Do nothing
+  }
 }
