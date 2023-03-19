dc032d2f0c711f00fad897708efb881b5bc9c6e4
Aaron Graybill <aarongraybill12@gmail.com>
2023-03-18 20:31:52 -0400

    fix function for unnamed input

--- R/show_colors.R
+++ R/show_colors.R
@@ -5,36 +5,41 @@
 show_colors <-
   function (colours=c(the$main_palette,the$accent_palette), labels = TRUE, borders = NULL, cex_label = 1,
           ncol = NULL){
-    if (is.null(names(colours))){scales::show_col(colours=colours,labels=labels,borders=borders,cex_label = cex_label,ncol=ncol)}
-  n <- length(colours)
-  ncol <- ceiling(sqrt(length(colours)))
-  nrow <- ceiling(n/ncol)
-  color_names=names(colours)
-  nas=(ncol*nrow)-length(colours)
-  color_names=c(color_names,rep(NA,nas))
-  if (is.list(colours)){colours <- unlist(colours)}
-  colours <- c(colours, rep(NA, nas))
-  colours <- matrix(colours, ncol = ncol, byrow = TRUE)
-  old <- par(pty = "s", mar = c(0, 0, 0, 0))
-  on.exit(par(old))
-  size <- max(dim(colours))
-  plot(c(0, size), c(0, -size), type = "n", xlab = "", ylab = "",
-       axes = FALSE)
-  rect(col(colours) - 1, -row(colours) + 1, col(colours),
-       -row(colours), col = colours, border = borders)
-  if (labels) {
-    if (is.null(color_names)){
-    print("I don't think you have names")
-    hcl <- farver::decode_colour(colours, "rgb", "hcl")
-    label_col <- ifelse(hcl[, "l"] > 50, "black", "white")
-    text(col(colours) - 0.5, -row(colours) + 0.5, colours,
-         cex = cex_label, col = label_col)
-    }
-    else {
+    colours <- unlist(colours)
+    if (is.null(names(colours))){
+      scales::show_col(colours=colours,labels=labels,borders=borders,cex_label = cex_label,ncol=ncol)
+      invisible()
+      } else{
+    n <- length(colours)
+    ncol <- ceiling(sqrt(length(colours)))
+    nrow <- ceiling(n/ncol)
+    color_names=names(colours)
+    nas=(ncol*nrow)-length(colours)
+    color_names=c(color_names,rep(NA,nas))
+    if (is.list(colours)){colours <- unlist(colours)}
+    colours <- c(colours, rep(NA, nas))
+    colours <- matrix(colours, ncol = ncol, byrow = TRUE)
+    old <- par(pty = "s", mar = c(0, 0, 0, 0))
+    on.exit(par(old))
+    size <- max(dim(colours))
+    plot(c(0, size), c(0, -size), type = "n", xlab = "", ylab = "",
+         axes = FALSE)
+    rect(col(colours) - 1, -row(colours) + 1, col(colours),
+         -row(colours), col = colours, border = borders)
+    if (labels) {
+      if (is.null(color_names)){
+      print("I don't think you have names")
       hcl <- farver::decode_colour(colours, "rgb", "hcl")
       label_col <- ifelse(hcl[, "l"] > 50, "black", "white")
-      text(col(colours) - 0.5, -row(colours) + 0.5, labels={matrix(color_names,nrow=nrow,byrow = T)},
+      text(col(colours) - 0.5, -row(colours) + 0.5, colours,
            cex = cex_label, col = label_col)
+      }
+      else {
+        hcl <- farver::decode_colour(colours, "rgb", "hcl")
+        label_col <- ifelse(hcl[, "l"] > 50, "black", "white")
+        text(col(colours) - 0.5, -row(colours) + 0.5, labels={matrix(color_names,nrow=nrow,byrow = T)},
+             cex = cex_label, col = label_col)
+        }
       }
     }
   }
