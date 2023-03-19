584a3041fb88c2cbaff5c00ede897bf5efa1d2e8
Aaron Graybill <aarongraybill12@gmail.com>
2023-03-19 18:35:46 -0400

    create function to get a random font from google

---
+++ R/font_shuffle.R
@@ -0,0 +1,13 @@
+font_shuffle <- function(){
+  font_info = sysfonts::font_info_google()
+  font_info = font_info[grep("latin",font_info$subsets),]
+  sans <- font_info[font_info$category=="sans-serif",]
+  sans <- sans$family[sample(1:nrow(sans),1)]
+
+  display <- font_info[font_info$category=="display",]
+  display <- display$family[sample(1:nrow(display),1)]
+
+  edit_the_fonts(sans=sans,display=display)
+}
+
+
