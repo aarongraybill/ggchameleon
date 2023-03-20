46d1a0c421ddf6713807d53f04c9cfa20bc28562
Aaron Graybill <aarongraybill12@gmail.com>
2023-03-20 17:46:59 -0400

    remove fonts without standard weight from shuffle

--- R/font_shuffle.R
+++ R/font_shuffle.R
@@ -1,11 +1,14 @@
 font_shuffle <- function(){
   font_info = sysfonts::font_info_google()
   font_info = font_info[grep("latin",font_info$subsets),]
+  font_info = font_info[grep("regular",font_info$variants),]
+
   sans <- font_info[font_info$category=="sans-serif",]
   sans <- sans$family[sample(1:nrow(sans),1)]
 
   display <- font_info[font_info$category=="display",]
   display <- display$family[sample(1:nrow(display),1)]
+
 
   edit_the_fonts(sans=sans,display=display)
 }
