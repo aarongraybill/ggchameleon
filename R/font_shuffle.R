0ebcc9d03873e3daab59f0c0ba8538e7b8edb9d7
Aaron Graybill <aarongraybill12@gmail.com>
2023-05-07 10:43:02 -0400

    popularity weighted font shuffle

--- R/font_shuffle.R
+++ R/font_shuffle.R
@@ -1,16 +1,17 @@
-font_shuffle <- function(){
-  font_info = sysfonts::font_info_google()
+font_shuffle <- function(exclude=c("Noto Sans ")){
+  font_info = font_popularity
   font_info = font_info[grep("latin",font_info$subsets),]
   font_info = font_info[grep("regular",font_info$variants),]
+  if (!is.null(exclude)){
+    font_info = font_info[!grepl(paste0(exclude,collapse="|"),font_info$family),]
+  }
 
-  sans <- font_info[font_info$category=="sans-serif",]
-  sans <- sans$family[sample(1:nrow(sans),1)]
+  sans <- font_info[font_info$category%in%c("sans-serif","monospace"),]
+  sans <- sample(sans$family,1,prob = log(sans$weekly))
 
   display <- font_info[font_info$category=="display",]
-  display <- display$family[sample(1:nrow(display),1)]
+  display <- sample(display$family,1,prob = log(display$weekly))
 
 
   edit_the_fonts(sans=sans,display=display)
 }
-
-
