f77740821503ab27d92f4c4ae6431eab631a5b95
Aaron Graybill <aarongraybill12@gmail.com>
2023-11-28 18:36:40 -0500

    check internet and remove docs

--- R/install_custom_fonts.R
+++ R/install_custom_fonts.R
@@ -1,20 +1,19 @@
-#' Get fonts that resemble the fonts we use for publication
-#'
-#' @description Pulls fonts from google then installs them into a file in the package location and then adds them to the list of R-available fonts
-#' @return Nothing,
-#' @details You cannot directly edit any other theme elements. This function returns a complete theme, and if you wish to further modify you should use the ggplot2::theme() command
-#' @examples
-#' install_philly_fonts()
-#'
-
 load_fonts <- function() {
 
-  for (font in the$fonts) {
+  if (!curl::has_internet()) {
+    warning("No internet connection, resorting to default fonts")
+    return(NULL)
+  }
+
+  get_font <- function(font){
     tryCatch(
       sysfonts::font_add_google(font, db_cache = T),
       error = function(x) {
         sysfonts::font_add_google(font, db_cache = F) #if we can't find it cached
       }
     )
+  }
+  for (font in the$fonts) {
+    get_font(font)
   }
 }
