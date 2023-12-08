8e63cbe75120526099068f4deec4bf30297d921d
Aaron Graybill <aarongraybill12@gmail.com>
2023-12-08 14:11:13 -0500

    remove unused dependencies

--- R/css.R
+++ R/css.R
@@ -72,7 +72,7 @@ make_html_header <- function(file_name){
   l1 <- "<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">"
   l2 <- "<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>"
   families <- paste0('family=',gsub(' ','\\+',the$fonts),collapse = "&")
-  l3 <- glue::glue("<link href=\"https://fonts.googleapis.com/css2?{families}&display=swap\" rel=\"stylesheet\">")
+  l3 <- paste0("<link href=\"https://fonts.googleapis.com/css2?",families,"&display=swap\" rel=\"stylesheet\">")
   htmltools::HTML(paste0(c(l1,l2,l3),collapse = '\n')) |> writeLines(con = file_name)
 
 }
