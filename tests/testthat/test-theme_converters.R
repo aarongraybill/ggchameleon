16e3f54094433390e30e87ffd209891e451586b2
Aaron Graybill <aarongraybill12@gmail.com>
2023-12-11 10:08:28 -0500

    test list to theme

--- tests/testthat/test-theme_converters.R
+++ tests/testthat/test-theme_converters.R
@@ -27,3 +27,35 @@ test_that("themes can be converted to lists", {
 
   expect_equal(l,expected)
 })
+
+test_that("lists can be converted to themes", {
+
+   t <-
+    list(
+      text = list(class = c("element_text","element"),
+                  attributes = list(
+                    family = NULL,
+                    face = NULL,
+                    colour = NULL,
+                    size = NULL,
+                    hjust = NULL,
+                    vjust = NULL,
+                    angle = 45,
+                    lineheight = NULL,
+                    margin = NULL,
+                    debug = NULL,
+                    inherit.blank = FALSE
+                  )),
+      legend.position = list(class = "character",
+                             attributes = list("none"))
+    ) |>
+    list_to_theme()
+
+   t <- rlang::exec(ggplot2::theme,!!!t)
+
+   expected <-
+       theme(legend.position = "none",
+             text = element_text(angle = 45))
+
+  expect_equal(t,expected)
+})
