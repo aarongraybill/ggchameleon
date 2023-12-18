228c743133c9a05507561d712bad0beea783a8a9
Aaron Graybill <aarongraybill12@gmail.com>
2023-12-17 19:44:10 -0500

    fetch raw data #74

--- data-raw/create_example_data.R
+++ data-raw/create_example_data.R
@@ -7,3 +7,13 @@
 # locally so that we can't lose it. Then we will extract the ids to create a
 # crosswalk. Then we'll do some data cleaning to make the data more easily
 # interpretable. Then we need to save the three data sets.
+
+# 1. Save data ----
+measurements_url <- 'https://raw.githubusercontent.com/alliebl/Drown-et-al/main/Chameleon_Measurements_v3.csv'
+download.file(measurement_url,'data-raw/measurements_raw.csv')
+
+color_change_url <- 'https://raw.githubusercontent.com/alliebl/Drown-et-al/main/Chameleon%20Color%20JNDs.csv'
+download.file(color_change_url,'data-raw/color_change_raw.csv')
+
+predation_url <- 'https://raw.githubusercontent.com/alliebl/Drown-et-al/main/Chameleon%20Behavior%20Proportions.csv'
+download.file(predation_url,'data-raw/predation_raw.csv')
