469aa34363b8c1c79714639a028ff105d076e6c3
Aaron Graybill <aarongraybill12@gmail.com>
2023-12-17 19:39:11 -0500

    data gameplan

---
+++ data-raw/create_example_data.R
@@ -0,0 +1,9 @@
+# There are three published datasets from Drown, Liebl, and Anderson (2022)
+# 1. Measurements
+# 2. Responses to simulated predation
+# 3. Amount of color change in the eyes of different animals
+#
+# These three are linkable by their internal ids. First, we'll save the raw data
+# locally so that we can't lose it. Then we will extract the ids to create a
+# crosswalk. Then we'll do some data cleaning to make the data more easily
+# interpretable. Then we need to save the three data sets.
